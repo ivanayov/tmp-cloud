4513f19e738142eb4eac2e9cf602b10b340f574d
Ivana Yovcheva <iyovcheva@vmware.com>
2018-03-09 15:59:16 +0200

    Add handler method to sentimentanalysis
    Signed-off-by: Ivana Yovcheva <iyovcheva@vmware.com>

--- sentimentanalysis/handler.py
+++ sentimentanalysis/handler.py
@@ -2,19 +2,8 @@ import sys
 import json
 from textblob import TextBlob
 
-# Set encoding to UTF-8 (vs ASCII to eliminate potential errors).
-reload(sys)
-sys.setdefaultencoding('utf8')
-
-def get_stdin():
-    buf = ""
-    for line in sys.stdin:
-        buf = buf + line
-    return buf
-
-if(__name__ == "__main__"):
-    st = get_stdin()
-    blob = TextBlob(st)
+def handle(req):
+    blob = TextBlob(req)
     res = {
         "polarity": 0,
         "subjectivity": 0
